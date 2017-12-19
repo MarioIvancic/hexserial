edb4ffd966843b9dfc97731ea6927b2972484c8a
Mario.Ivancic <mario.ivancic@mtel.ba>
2017-12-19 10:38:04 +0100

    1.0.4

--- ArmSerial.c
+++ ArmSerial.c
@@ -1,5 +1,5 @@
 /**************************************************************
-Version 1.03
+Version 1.04
 
 1.00 - Modifikovano tako da zadnji generisani serijski broj cuva u 
 registry bazi. Serijski brojevi u bezi zavise od prefixa i 
@@ -24,9 +24,22 @@ prefix duzi odsjeca se na 5 karaktera.
 0000000001 do 4294967295. 
 Sve zajedno: PPPPP-YYYYMMDDHHmmSS-RRRRRRRRRR
 
+1.04 - Modifikovan je format serijskog broja tako sto je povecana duzina
+prefixa sa 5 na 9 karaktera a smanjena duzina serijskog broja sa 10 na 6 cifara.
+U komandnom fajlu se # ili ; na pocetku linije koriste da oznace komentar
+
+Serijski broj se sastoji od rednog broja, timestamp stringa i proizvoljnog
+prefixa.
+- Prefix moze biti bilo koji string bez praznina do 9 karaktera duzine. Ako je
+prefix duzi odsjeca se na 9 karaktera.
+- Time stamp sadrzi 14 ASCII cifara i ima format YYYYMMDDHHMMSS.
+- Redni broj se sastoji od 6 decimalnih cifara i moze biti u opsegu od 
+000001 do 999999. 
+Sve zajedno: PPPPPPPPP-YYYYMMDDHHmmSS-RRRRRR
+
 **************************************************************/
 
-#define VERSION_STRING "1.03"
+#define VERSION_STRING "1.04"
 
 // include needed header files
 #include <windows.h>
@@ -60,7 +73,7 @@ unsigned int forced_serial = 0;
 char serialnumber[32];
 
 // prefix serijskog broja
-char serialprefix[8];
+char serialprefix[16];
 
 // function to generate the actual serial number and put it into
 // an intel hex record ready for programming
@@ -74,9 +87,9 @@ int generate(FILE *outputfile)
 	struct tm* timedate;
 
 	// formira se registry path
-	strcpy(regpath,REG_PATH);
-	strcat(regpath,"\\");
-	strcat(regpath,serialprefix);
+	strcpy(regpath, REG_PATH);
+	strcat(regpath, "\\");
+	strcat(regpath, serialprefix);
 
 	r = RegOpenKeyEx(
 		HKEY_LOCAL_MACHINE,
@@ -84,6 +97,7 @@ int generate(FILE *outputfile)
 		REG_OPTION_NON_VOLATILE,
 		KEY_ALL_ACCESS,
 		&key);
+
 	if(r) // nije uspjelo -- valjda ne postoji taj kljuc u ragistry bazi
 	{
 		// kraira se kljuc u bazi
@@ -99,6 +113,7 @@ int generate(FILE *outputfile)
 			0,
 			&key,
 			&disp);
+
 		if(r) // ako ni ovo ne uspije odustajem
 		{
 			return -1;
@@ -106,6 +121,7 @@ int generate(FILE *outputfile)
 
 		if(force && update) serial = forced_serial;
 		else serial = 1;
+
 		r = RegSetValueEx(
 			key,
 			REG_VALUE,
@@ -135,11 +151,13 @@ int generate(FILE *outputfile)
 			&type,
 			(unsigned char*)&serial,
 			&datasize);
+
 		if(r || (datasize != 4) || (type != REG_DWORD))
 		{
 			RegCloseKey(key);
 			return -1;
 		}
+
 		// zadnji serijski broj je u serial
 		if(increment) serial++; // za 1 veci
 
@@ -167,12 +185,13 @@ int generate(FILE *outputfile)
 
 	// get the current time
 	time((time_t *)&now);
+
 	timedate = localtime(&now);
 	
 	// store current time in serial number array
 	sprintf(
 		serialnumber,
-		"%s-%04d%02d%02d%02d%02d%02d-%010u",
+		"%s-%04d%02d%02d%02d%02d%02d-%06u",
 		serialprefix,
 		1900 + timedate->tm_year,
 		1 + timedate->tm_mon,
@@ -219,9 +238,9 @@ int main(int argc, char **argv)
 	char serialaddress[16];
 	int ret_code = 1; // OK
 	int print = 0;
+	int line_count; // broji linije u komandnom fajlu
 
-	// if we didn't receive two arguments then we can't do
-	// anything
+	// if we didn't receive two arguments then we can't do anything
 	if(argc < 2)
 	{
 		fprintf(stderr,"ArmSerial, version %s, build date: %s\n",VERSION_STRING,__DATE__);
@@ -237,6 +256,7 @@ int main(int argc, char **argv)
 		fprintf(stderr,"absolute path to user file\n");
 		fprintf(stderr,"serial string prefix (up to 5 chars)\n");
 		fprintf(stderr,"serial string address in HEX file (in HEX notation)\n");
+		fprintf(stderr,"Line starting with '#' or ';' is treated as comment\n");
 		return ! ret_code; // FAIL
 	}
 	while(argc > 2)
@@ -269,38 +289,51 @@ int main(int argc, char **argv)
 	commandfile = fopen(argv[1], "r");
 	if (!commandfile) return ! ret_code;
 
-	// try to read in the path to the file to create with hex
-	// records
-	if(fgets(datafilepath, 2000, commandfile) == NULL)
+	line_count = 0;
+
+	while(!feof(commandfile))
 	{
-		fclose(commandfile);
-		return ! ret_code;
+		char read_buffer[2000]; // za citanje komandnog fajla
+
+		if(fgets(read_buffer, 2000, commandfile) == NULL)
+		{
+			fclose(commandfile);
+			return ! ret_code;
+		}
+
+		// da li je komentar
+		if(read_buffer[0] == '#') continue;
+		if(read_buffer[0] == ';') continue;
+
+		switch(line_count)
+		{
+		case 0: // datafilepath
+			// try to read in the path to the file to create with hex records
+			memcpy(datafilepath, read_buffer, 2000);
+			line_count++;
+			break;
+		
+		case 1: // userfilepath
+			// try to read in the path to the user output file to create
+			memcpy(userfilepath, read_buffer, 2000);
+			line_count++;
+			break;
+
+		case 2: // try to read in the serial prefix
+			memcpy(serialprefix, read_buffer, 16);
+			serialprefix[9] = 0; // ogranicava se prefix na 9 karaktera
+			line_count++;
+			break;
+
+		case 3: // try to read in the serial address
+			memcpy(serialaddress, read_buffer, 16);
+			line_count++;
+			break;
+
+		default: break;
+		}
 	}
-
-	// try to read in the path to the user output file to
-	// create
-	if(fgets(userfilepath, 2000, commandfile) == NULL)
-	{
-		fclose(commandfile);
-		return ! ret_code;
-	}
-
-
-	// try to read in the serial prefix
-	if(fgets(serialprefix, 8, commandfile) == NULL)
-	{
-		fclose(commandfile);
-		return ! ret_code;
-	}
-	serialprefix[5] = 0; // ogranicava se prefix na 5 karaktera
-
-	// try to read in the serial address
-	if(fgets(serialaddress, 16, commandfile) == NULL)
-	{
-		fclose(commandfile);
-		return ! ret_code;
-	}
-
+	
 	// we are done with the command file
 	fclose(commandfile);
 
@@ -329,7 +362,6 @@ int main(int argc, char **argv)
 	// generate some feedback for the user
 	// in this case we tell the user what the serial number generated
 	// is so it can be noted and maybe put on a sticker somewhere
-//	fprintf(userfile, "Serial Number: %s", serialnumber);
 	fprintf(userfile, "%s", serialnumber);
 	fclose(userfile);
 
